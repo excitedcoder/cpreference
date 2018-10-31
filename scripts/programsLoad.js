5bdecf5b497f5ee0adc49c1443970573c78ee9ce
Ajay Kumar Bollepalli <ajay.bollepalli@gmail.com>
2018-10-31 12:20:36 +0530

    programs menu created. general programs page now using bootstrap

--- scripts/programsLoad.js
+++ scripts/programsLoad.js
@@ -1,6 +1,18 @@
 $(document).ready( () => {
     document.getElementById('dataTable').style.visibility = "hidden";
 });
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBs5aJA-ytz8k6ych2oBRNbFcbCowxxpac",
+    authDomain: "cpreference-46d14.firebaseapp.com",
+    databaseURL: "https://cpreference-46d14.firebaseio.com",
+    projectId: "cpreference-46d14",
+    storageBucket: "cpreference-46d14.appspot.com",
+    messagingSenderId: "309468014422"
+};
+firebase.initializeApp(config);
+
 var ref = firebase.database().ref().child("programs");
 var x = 1;
 ref.on("child_added", data => {
@@ -13,7 +25,7 @@ ref.on("child_added", data => {
         tr.append("<td>" + x + "</td>");
         tr.append("<td>" + data.key + "</td>");
         tr.append("<td>" + element["Title"] + "</td>");        
-        tr.append("<td> <a href='" + element["Link"] + "' class='ui compact primary button' target='_blank'>See Execution</a></td>");
+        tr.append("<td> <a href='" + element["Link"] + "' class='btn btn-primary btn-sm' target='_blank'>See Execution</a></td>");
 
         tbody.append(tr);
         x++;
